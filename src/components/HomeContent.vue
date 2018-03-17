6b07bd48cec5b4f03ffc33e38dba72d142e83e4c
Paweł P <panczo12d@gmail.com>
2018-03-17 11:17:43 +0100

    news at homepage

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+        <news-component></news-component>
+        <news-component></news-component>
+        <news-component></news-component>
+      </div>
+      <div class="col-4">col-4</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewsComponent from '@/components/News'
+
+export default {
+  components: {
+    newsComponent: NewsComponent
+  }
+}
+</script>
