577d4f506598578a0f130926efcdb2b4cbd884c6
Paweł P <panczo12d@gmail.com>
2018-03-22 12:52:53 +0100

    move to external component

---
+++ src/components/News.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="card mb-3">
+      <img class="card-img-top" src="../../static/images/2.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title"> {{this.title}} </h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'newsBody', 'newsDateTime']
+}
+</script>
