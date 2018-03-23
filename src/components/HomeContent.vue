202ee5c1c8ec79568ad5bbf6cebc3503be8076e7
Paweł P <panczo12d@gmail.com>
2018-03-23 21:16:46 +0100

    removed left padding on col-8 homepage

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-8 pl-0">
+      <div class="col-8 px-0">
         <div  v-if="newsLoaded">
         <news-component v-for="item in news" v-bind:key="item.id"
           :title="item.title" :newsBody="item.body" :newsDateTime="item.dateTime"></news-component>
