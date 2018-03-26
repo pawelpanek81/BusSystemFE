cf9b821b809d2f1853aecac2147deccd38a96880
maaaaagda <magdapolak96@gmail.com>
2018-03-26 19:00:13 +0200

    changed menu to one responsive, ! hiding dropdown after click not working

--- src/pages/AdminPanel.vue
+++ src/pages/AdminPanel.vue
@@ -1,40 +1,28 @@
 <template>
   <div>
     <div class="container mt-4 mb-4">
-      <div v-if="$mq.resize && $mq.above('576px')">
-        <div class="row">
-          <div class="col-sm-4 col-md-3">
-            <div>
-              <div class="vertical-menu" >
-                <a href="#" class="active">Kierowcy</a>
-                <a href="#">Przystanki</a>
-                <a href="#">Trasy</a>
-                <a href="#">Linie autobusowe</a>
-                <a href="#">Rozkłady jazdy</a>
-                <a href="#">Pojazdy</a>
+      <div class="row">
+        <div class="col-12 col-md-4 col-lg-3">
+          <div>
+            <nav class="navbar navbar-expand-md vertical-menu m-0 p-0">
+              <button class="navbar-toggler" type="button" id="button-toggle" data-toggle="collapse" href="#verticalNavbar"
+                      aria-controls="verticalNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                Przejdź do...        <i class="fas fa-caret-down float-right"></i>
+              </button>
+              <div class="collapse navbar-collapse" id="verticalNavbar">
+                <div class="vertical-menu" >
+                  <a href="#" class="active">Kierowcy</a>
+                  <a href="#">Przystanki</a>
+                  <a href="#">Trasy</a>
+                  <a href="#">Linie autobusowe</a>
+                  <a href="#">Rozkłady jazdy</a>
+                  <a href="#">Pojazdy</a>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-9"></div>
-        </div>
-      </div>
-      <div v-else>
-        <div class="container">
-          <div class="btn-group vertical-menu">
-            <button type="button" class="btn btn-success btn-width">Przejdź do...</button>
-            <button class="btn btn-success dropdown-toggle dropdown-toggle-split" data-toggle="collapse" href="#collapseExample" role="button"
-                    aria-expanded="false" aria-controls="collapseExample">
-            </button>
-          </div>
-          <div class="collapse vertical-menu" id="collapseExample">
-            <a href="#" class="active">Kierowcy</a>
-            <a href="#">Przystanki</a>
-            <a href="#">Trasy</a>
-            <a href="#">Linie autobusowe</a>
-            <a href="#">Rozkłady jazdy</a>
-            <a href="#">Pojazdy</a>
+            </nav>
           </div>
         </div>
+        <div class="col-md-6 col-lg-9"></div>
       </div>
     </div>
   </div>
@@ -45,12 +33,14 @@
     width: 100%;
   }
 
-  .vertical-menu a {
+  .vertical-menu a, .vertical-menu button {
     background-color: #eee;
     color: black;
     display: block;
     padding: 12px;
     text-decoration: none;
+    outline: none;
+    width: 100%;
   }
 
   .vertical-menu a:hover {
@@ -58,11 +48,17 @@
     cursor: pointer;
   }
   .vertical-menu a.active {
-    background-color: #4CAF50; /* Add a green color to the "active/current" link */
+    background-color: #4CAF50;
     color: white;
   }
   .btn-width {
     width: 95%;
+  }
+  .ul {
+    display: block;
+  }
+  .no-outline {
+    outline: none;
   }
 </style>
 
@@ -71,7 +67,7 @@ export default {
   updated () {
     $('.vertical-menu a').on('click', function () {
       $(this).addClass('active').siblings().removeClass('active')
-      $('#collapseExample').collapse('hide')
+      $('#button-toggle').click()
     })
   }
 }
