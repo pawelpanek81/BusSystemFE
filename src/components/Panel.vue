e7e13407904c88b43baf35ba01ed6b2e28b242d6
maaaaagda <magdapolak96@gmail.com>
2018-03-26 19:49:14 +0200

    did a little clean up, created one panel component for different panels

---
+++ src/components/Panel.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="container mt-4 mb-4">
+      <div class="row">
+        <div class="col-12 col-md-4 col-lg-3">
+          <div>
+            <nav class="navbar navbar-expand-md vertical-menu m-0 p-0">
+              <button class="navbar-toggler" type="button" id="button-toggle" data-toggle="collapse"
+                      href="#verticalNavbar"
+                      aria-controls="verticalNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                Przejdź do...<i class="fas fa-caret-down float-right"></i>
+              </button>
+              <div class="collapse navbar-collapse" id="verticalNavbar">
+                <div class="vertical-menu">
+                  <a v-for="item in items" v-bind:key="item.name">{{item.name}}</a>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-9">
+          <div class="m-2">
+            <h5>{{panelName}}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['items', 'panelName'],
+  mounted () {
+    $('.vertical-menu a:first').addClass('active')
+  },
+  updated () {
+    $('.vertical-menu a').on('click', function () {
+      $(this).addClass('active').siblings().removeClass('active')
+      $('#button-toggle').click()
+    })
+  }
+}
+</script>
+
+<style>
+  .vertical-menu {
+    width: 100%;
+  }
+
+  .vertical-menu a, .vertical-menu button {
+    background-color: #eee;
+    color: black;
+    display: block;
+    padding: 12px;
+    text-decoration: none;
+    outline: none;
+    width: 100%;
+  }
+
+  .vertical-menu a:hover {
+    background-color: #ccc;
+    cursor: pointer;
+  }
+
+  .vertical-menu a.active {
+    background-color: #4CAF50;
+    color: white;
+  }
+
+</style>
--- src/pages/AdminPanel.vue
+++ src/pages/AdminPanel.vue
@@ -1,74 +1,25 @@
 <template>
-  <div>
-    <div class="container mt-4 mb-4">
-      <div class="row">
-        <div class="col-12 col-md-4 col-lg-3">
-          <div>
-            <nav class="navbar navbar-expand-md vertical-menu m-0 p-0">
-              <button class="navbar-toggler" type="button" id="button-toggle" data-toggle="collapse" href="#verticalNavbar"
-                      aria-controls="verticalNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                Przejdź do...        <i class="fas fa-caret-down float-right"></i>
-              </button>
-              <div class="collapse navbar-collapse" id="verticalNavbar">
-                <div class="vertical-menu" >
-                  <a href="#" class="active">Kierowcy</a>
-                  <a href="#">Przystanki</a>
-                  <a href="#">Trasy</a>
-                  <a href="#">Linie autobusowe</a>
-                  <a href="#">Rozkłady jazdy</a>
-                  <a href="#">Pojazdy</a>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-9"></div>
-      </div>
-    </div>
-  </div>
+  <panel :items="items" :panelName="panelName"></panel>
 </template>
 
-<style>
-  .vertical-menu {
-    width: 100%;
-  }
+<script>
+import panel from '@/components/Panel'
 
-  .vertical-menu a, .vertical-menu button {
-    background-color: #eee;
-    color: black;
-    display: block;
-    padding: 12px;
-    text-decoration: none;
-    outline: none;
-    width: 100%;
-  }
-
-  .vertical-menu a:hover {
-    background-color: #ccc;
-    cursor: pointer;
-  }
-  .vertical-menu a.active {
-    background-color: #4CAF50;
-    color: white;
-  }
-  .btn-width {
-    width: 95%;
-  }
-  .ul {
-    display: block;
-  }
-  .no-outline {
-    outline: none;
-  }
-</style>
-
-<script scoped>
 export default {
-  updated () {
-    $('.vertical-menu a').on('click', function () {
-      $(this).addClass('active').siblings().removeClass('active')
-      $('#button-toggle').click()
-    })
+  components: {
+    panel
+  },
+  data: function () {
+    var items = [
+      {name: 'Kierowcy', urlLink: ''},
+      {name: 'Przystanki', urlLink: ''},
+      {name: 'Trasy', urlLink: ''},
+      {name: 'Linie autobusowe', urlLink: ''},
+      {name: 'Rozkłady jazdy', urlLink: ''},
+      {name: 'Pojazdy', urlLink: ''}
+    ]
+    var panelName = 'AdministrationPanel'
+    return {items, panelName}
   }
 }
 </script>
