d4de5fb3c09781ad62ef2378468e8d3e14f71690
Paweł P <panczo12d@gmail.com>
2018-04-07 01:24:40 +0200

    live herokut test commit
    checking env to determine api server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_BASE_URL: localStorage.getItem('API_BASE_URL'),
-  API_LOGIN_BASE_URL: localStorage.getItem('API_LOGIN_BASE_URL')
+  API_BASE_URL: process.env.NODE_ENV === 'production'
+    ? 'http://januszpol-rest.herokuapp.com/api/v1.0' : 'http://localhost:3000/api/v1.0',
+  API_LOGIN_BASE_URL: process.env.NODE_ENV === 'production'
+    ? 'http://januszpol-rest.herokuapp.com' : 'http://localhost:3000'
 }
-console.log(localStorage.getItem('API_LOGIN_BASE_URL'))
