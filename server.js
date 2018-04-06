5e51c0d4fc6b6b6843314108b69508b3dfad2871
Paweł P <panczo12d@gmail.com>
2018-04-06 22:15:53 +0200

    fix mappings at express server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var app = express();
 app.use(serveStatic(__dirname + '/dist'));
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
