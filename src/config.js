6a3adc7f409480796231664c2650b31eb4daa711
Paweł P <panczo12d@gmail.com>
2018-04-07 01:00:04 +0200

    heroku debuging

--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ export default {
   API_BASE_URL: localStorage.getItem('API_BASE_URL'),
   API_LOGIN_BASE_URL: localStorage.getItem('API_LOGIN_BASE_URL')
 }
+console.log(localStorage.getItem('API_LOGIN_BASE_URL'))
