086398a81f24a85d9b4dc387e36947636a8627d9
maaaaagda <magdapolak96@gmail.com>
2018-04-12 16:44:44 +0200

    displaying bus stops by clicking on line in list of bus lines

---
+++ src/components/admin/panel/AllBusLines.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-8">
+        <h5>Linie autobusowe</h5>
+      </div>
+      <div class="col-4 mb-3">
+        <button type="button"  class="btn btn-outline-success">
+          Dodaj nową linię
+        </button>
+      </div>
+    </div>
+    <div class="row">
+      <table v-if="busLinesLoaded" class="table table-hover" id="allbusLines">
+        <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nazwa</th>
+          <th scope="col">Z</th>
+          <th scope="col">Do</th>
+          <th scope="col">Czas przejazdu</th>
+          <th scope="col">Więcej</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="busLine in busLines" v-bind:key="busLine.id">
+          <th scope="row">{{busLine.id}}</th>
+          <td>{{busLine.name}}</td>
+          <td>{{busLine.from.city}}</td>
+          <td>{{busLine.to.city}}</td>
+          <td>{{busLine.driveTime}}</td>
+          <td>
+            <router-link :to="{path: `/admin/buslines/${busLine.id}/route`, params: {lineId: busLine.id}}">
+              <button class="btn btn-outline-success">
+                Więcej
+              </button>
+            </router-link>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import api from '../../../api/endpoints'
+
+export default {
+  data () {
+    return {
+      busLinesLoaded: false,
+      busLines: []
+    }
+  },
+  methods: {
+    getBusLines () {
+      axios.get(api.BUS_LINES)
+        .then((response) => {
+          this.busLinesLoaded = true
+          this.busLines = response.data
+        })
+        .catch(function () {})
+    }
+  },
+  mounted () {
+    this.getBusLines()
+  }
+}
+</script>
--- src/components/admin/panel/Route.vue
+++ src/components/admin/panel/Route.vue
@@ -1,19 +1,34 @@
 <template>
-  <div>
-    <h5>Linia nr P3 Wrocław <i class="fas fa-long-arrow-alt-right"></i> Lublin</h5>
-    <div class="row" v-for="busStop in busStops" v-bind:key="busStop.id">
+  <div v-if="busLineLoaded">
+    <div class="row">
+      <div class="col-8">
+        <h5>Linia nr P3 {{busLine.from.city}} <i class="fas fa-long-arrow-alt-right"></i> {{busLine.to.city}} </h5>
+      </div>
+      <div class="col-4">
+        <router-link to="/admin/buslines">
+          <button type="button" class="btn btn-outline-success">
+            Pokaż wszystkie linie
+          </button>
+        </router-link>
+      </div>
+    </div>
+    <div v-if="busStopsLoaded" class="row container" v-for="busStop in busStops" v-bind:key="busStop.id">
       <div class="busStop mr-2"/>
       <div class="align-self-center">
-        {{busStop.city}}, {{busStop.address}}
+        {{busStop.busStop.city}} {{busStop.busStop.address}}
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import api from '../../../api/endpoints'
+
 export default {
   data () {
     return {
+      lineId: null,
       busStops: [
         {
           id: 0,
@@ -40,8 +55,34 @@ export default {
           name: 'Zoo Lublin'
         }
       ],
-      busStops2: []
+      busStops2: [],
+      busLine: {},
+      busLineLoaded: false,
+      busStopsLoaded: false
     }
+  },
+  methods: {
+    getBusLine (id) {
+      axios.get(`${api.BUS_LINES}/${id}`)
+        .then((response) => {
+          this.busLine = response.data
+          this.busLineLoaded = true
+        })
+    },
+    getBusStops (id) {
+      axios.get(`${api.BUS_LINES}/${id}/routes`)
+        .then((response) => {
+          this.busStops = response.data
+          this.busStopsLoaded = true
+        })
+    }
+  },
+  created () {
+    this.lineId = this.$route.params.id
+  },
+  mounted () {
+    this.getBusLine(this.lineId)
+    this.getBusStops(this.lineId)
   }
 }
 </script>
