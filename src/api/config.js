d791f5d56f7836530b205f0f0d1f62f709afee8e
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-04-18 00:20:39 +0200

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_BASE_URL: 'http://januszpol-rest.herokuapp.com/api/v1.0',
-  API_LOGIN_BASE_URL: 'http://januszpol-rest.herokuapp.com'
+  API_BASE_URL: process.env.NODE_ENV === 'production'
+    ? 'http://januszpol-rest.herokuapp.com/api/v1.0' : 'http://localhost:3000/api/v1.0',
+  API_LOGIN_BASE_URL: process.env.NODE_ENV === 'production'
+    ? 'http://januszpol-rest.herokuapp.com' : 'http://localhost:3000'
 }
