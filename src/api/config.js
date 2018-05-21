bfc191e98204216423a7510bc67c4b6f481a689e
Paweł P <panczo12d@gmail.com>
2018-05-21 23:40:34 +0200

    reverted config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_BASE_URL: 'http://localhost:3000/api/v1.0',
-  API_LOGIN_BASE_URL: 'http://localhost:3000'
+  API_BASE_URL: 'http://januszpol-rest.herokuapp.com/api/v1.0',
+  API_LOGIN_BASE_URL: 'http://januszpol-rest.herokuapp.com'
 }
